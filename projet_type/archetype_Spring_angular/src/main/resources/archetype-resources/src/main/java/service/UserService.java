5bc06eee925eee9ae5d82b6f42e5e137d54b9eb4
Bérenger Thévenet <berenger.thevenet@laposte.net>
2018-03-19 11:37:41 +0100

    Ajout projet-type

---
+++ projet_type/archetype_Spring_angular/src/main/resources/archetype-resources/src/main/java/service/UserService.java
@@ -0,0 +1,30 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package}.service;
+
+import java.util.List;
+
+import ${package}.model.User;
+
+
+
+public interface UserService {
+	
+	User findById(long id);
+	
+	User findByName(String name);
+	
+	void saveUser(User user);
+	
+	void updateUser(User user);
+	
+	void deleteUserById(long id);
+
+	List<User> findAllUsers(); 
+	
+	void deleteAllUsers();
+	
+	public boolean isUserExist(User user);
+	
+}
---
+++ projet_type/archetype_Spring_angular/target/test-classes/projects/basic/project/basic/src/main/java/service/UserService.java
@@ -0,0 +1,27 @@
+package it.pkg.service;
+
+import java.util.List;
+
+import it.pkg.model.User;
+
+
+
+public interface UserService {
+	
+	User findById(long id);
+	
+	User findByName(String name);
+	
+	void saveUser(User user);
+	
+	void updateUser(User user);
+	
+	void deleteUserById(long id);
+
+	List<User> findAllUsers(); 
+	
+	void deleteAllUsers();
+	
+	public boolean isUserExist(User user);
+	
+}
